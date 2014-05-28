e4a2b9c29b8ef30897e261d9eb82b1e795e62841
Andesy <andes@Andrews-MacBook-Pro.local>
2014-05-28 18:39:26 -0400

    more tests

--- test/spec/hilarySpec.js
+++ test/spec/hilarySpec.js
@@ -2,6 +2,9 @@ describe("hilary", function() {
   var container,
       testModuleDefinitions;
 
+  window.externalWindowComponent1 = function() { return 'externalWindowComponent1'; };
+  window.externalWindowComponent2 = function() { return 'externalWindowComponent2'; };
+
   testModuleDefinitions = {
     empty: {
       name: 'foo',
@@ -17,14 +20,35 @@ describe("hilary", function() {
     container = hilary.createContainer();
   });
 
+  describe('hilary.ctor', function(){
+    it('should exist in window', function() {
+      expect(window.hilary).not.toBe(null);
+    });
 
-  it('should exist in window', function() {
-    expect(window.hilary).not.toBe(null);
-  });
+    it('should create new parent containers', function() {
+      expect(container).not.toBe(null);
+    });
 
-  it('should create new parent containers', function() {
-    expect(container).not.toBe(null);
-  });
+    it('should create child containers', function() {
+      var _child = container.createChildContainer();
+
+      container.register(testModuleDefinitions.empty.name, function() {
+        return testModuleDefinitions.empty.output;
+      });
+
+      _child.register(testModuleDefinitions.emptyToo.name, function() {
+        return testModuleDefinitions.emptyToo.output;
+      });
+
+      var shouldThrow = function () {
+        return container.resolve(testModuleDefinitions.emptyToo.name);
+      };
+
+      expect(container.resolve(testModuleDefinitions.empty.name)()).toBe(testModuleDefinitions.empty.output);
+      expect(_child.resolve(testModuleDefinitions.emptyToo.name)()).toBe(testModuleDefinitions.emptyToo.output);
+      expect(shouldThrow).toThrow();
+    });
+  }); // / ctor
 
   describe('hilary.register', function(){
     it('should register single modules by name', function() {
@@ -34,7 +58,17 @@ describe("hilary", function() {
 
       expect(container.resolve(testModuleDefinitions.empty.name)()).toBe(testModuleDefinitions.empty.output);
     });
-  }); 
+
+    it('should register any number of modules by func', function() {
+      container.register(function(cntr) {
+        cntr.hello = function() { return 'world' };
+        cntr.ola = function() { return 'el mundo' };
+      });
+
+      expect(container.resolve('hello')()).toBe('world');
+      expect(container.resolve('ola')()).toBe('el mundo');
+    });
+  }); // /register
 
   describe('hilary.resolve', function(){
 
@@ -62,13 +96,30 @@ describe("hilary", function() {
     });
 
     it('should resolve container and parentContainer modules by name', function() {
-      container.createChildContainer().resolve(['$container', '$parentContainer'], function (ctnr, parent) {
+      container.createChildContainer().resolve(['hilary::container', 'hilary::parentContainer'], function (ctnr, parent) {
         expect(ctnr).not.toBe(null);
         expect(ctnr.getContainer).not.toBe(null);
         expect(parent).not.toBe(null);
         expect(parent.getContainer).not.toBe(null);
       });
     });
-  });
+
+    it('should resolve through parent hierarchy', function() {
+      var _child = container.createChildContainer();
+      var _grandChild = _child.createChildContainer();
+
+      container.register(testModuleDefinitions.empty.name, function() {
+        return testModuleDefinitions.empty.output;
+      });
+
+      _child.register(testModuleDefinitions.emptyToo.name, function() {
+        return testModuleDefinitions.emptyToo.output;
+      });
+
+      expect(_grandChild.resolve(testModuleDefinitions.empty.name)()).toBe(testModuleDefinitions.empty.output);
+      expect(_grandChild.resolve(testModuleDefinitions.emptyToo.name)()).toBe(testModuleDefinitions.emptyToo.output);
+    });
+
+  }); // /resolve
 
 });
