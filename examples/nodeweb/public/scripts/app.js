96214b2167b5553bf898a9596f03539c910bbf02
&y <andes.collab@gmail.com>
2015-02-16 23:10:32 -0500

    updates version

--- examples/nodeweb/public/scripts/app.js
+++ examples/nodeweb/public/scripts/app.js
@@ -1,4 +1,8 @@
 /*globals spa, console*/
+window.windowFoo = {
+    isWindowFoo: true
+};
+
 spa.resolve(function (resolve, exports, window) {
     "use strict";
     
@@ -8,10 +12,12 @@ spa.resolve(function (resolve, exports, window) {
     compose = function (container) {
         container.register('myFactory', function () {
             var module1 = resolve('module1'),
-                module2 = resolve('module2');
+                module2 = resolve('module2'),
+                windowFoo = resolve('windowFoo');
             
             console.log('module1 loaded:', module1.isModule1);
             console.log('module2 loaded:', module2.isModule2);
+            console.log('windowFoo loaded:', windowFoo.isWindowFoo);
             
             return {
                 module1: module1,
