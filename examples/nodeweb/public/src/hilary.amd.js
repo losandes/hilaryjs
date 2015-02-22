57afd9b98124d960a63f397a0ea0ceb9f046cd5f
&y <andes.collab@gmail.com>
2015-02-21 22:35:40 -0500

    amd extension for v2

--- examples/nodeweb/public/src/hilary.amd.js
+++ examples/nodeweb/public/src/hilary.amd.js
@@ -6,11 +6,77 @@
     var AMDContainer;
     
     Hilary.extend('define', function (scope) {
-        return scope.amd.register;
+        var context = scope.getContext(),
+            utils = context.utils,
+            err = context.exceptionHandlers;
+        
+        return function (moduleName, dependencies, factory) {
+            if (utils.isFunction(factory)) {
+                // all 3 arguments are present
+                return scope.register({
+                    name: moduleName,
+                    dependencies: dependencies,
+                    factory: factory
+                });
+            } else if (utils.isString(moduleName) && utils.isFunction(dependencies)) {
+                // the factory was passed in as the second argument - no dependencies exist
+                // moduleName == moduleName and dependencies == factory
+                return scope.register({
+                    name: moduleName,
+                    factory: dependencies
+                });
+            } else if (utils.isArray(moduleName) && utils.isFunction(dependencies)) {
+                // anonymous definition: the factory was passed in as the second argument - dependencies exist
+                // moduleName == dependencies and dependencies == factory
+                return scope.autoResolve({
+                    dependencies: moduleName,
+                    factory: dependencies
+                });
+            } else if (utils.isFunction(moduleName)) {
+                // anonymous definition: the factory was passed in as the first argument
+                return scope.require(moduleName);
+            } else if (utils.isObject(moduleName)) {
+                // anonymous definition: an object literal was passed in as the first argument
+                var prop;
+
+                for (prop in moduleName) {
+                    if (moduleName.hasOwnProperty(prop)) {
+                        scope.register({
+                            name: prop,
+                            factory: moduleName[prop]
+                        });
+                    }
+                }
+                return scope;
+            } else if (utils.isString(moduleName) && utils.isObject(dependencies)) {
+                // the factory in an object literal and was passed in as the second argument - no dependencies exist
+                // moduleName == moduleName and dependencies == object literal
+                return scope.register({
+                    name: moduleName,
+                    factory: dependencies
+                });
+            } else {
+                throw err.argumentException('A factory function was not found to define ' + moduleName, 'factory');
+            }
+        };
     });
     
     Hilary.extend('require', function (scope) {
-        return scope.amd.resolve;
+        var context = scope.getContext(),
+            utils = context.utils;
+        
+        return function (dependencies, factory) {
+            if (utils.isFunction(dependencies)) {
+                // The first argument is the factory
+                return dependencies(scope.require, context.container, (window || module));
+            } else if (typeof dependencies === 'string') {
+                // A single module is being required
+                return scope.resolve(dependencies);
+            } else {
+                // An array of dependencies are being required for the factory
+                return scope.resolveMany(dependencies, factory);
+            }
+        };
     });
     
     AMDContainer = new Hilary();
--- test/browser/hilary.browser.amd.fixture.js
+++ test/browser/hilary.browser.amd.fixture.js
@@ -1,173 +1,166 @@
 /*globals require*/
-require(['describe', 'beforeEach', 'Hilary', 'it', 'expect'], function (describe, beforeEach, Hilary, it, expect) {
+
+window['hilary.browser.amd.fixture'] = function () {
     "use strict";
-
-    describe("Hilary AMD Extensions", function () {
-        var container,
-            global = window,
-            testModuleDefinitions = {
-                empty: {
-                    name: 'foo',
-                    output: 'registered foo!'
-                },
-                emptyToo: {
-                    name: 'bar',
-                    output: 'registered bar!'
-                }
-            };
-
-        beforeEach(function () {
-            container = new Hilary();
-        });
-
-        describe('AMDContainer', function () {
-            it('should exist in window', function () {
-                expect(global.AMDContainer).toBeDefined();
-            });
-        });
-
-        describe('define', function () {
-            it('should exist in window', function () {
-                expect(global.define).toBeDefined();
-                expect(global.define.amd).toBeDefined();
-            });
-
-            it('should exist in new Hilary instances', function () {
-                expect(container.define).toBeDefined();
-            });
-        });
-
-        describe('require', function () {
-            it('should exist in window', function () {
-                expect(global.require).toBeDefined();
-            });
-
-            it('should exist in new Hilary instances', function () {
-                expect(container.require).toBeDefined();
-            });
-        });
-
-        describe('define, when registering modules', function () {
-            it('should be able to define the module by name WITHOUT dependencies', function () {
-                var sut = global.define(testModuleDefinitions.empty.name, function () {
-                    return testModuleDefinitions.empty.output;
-                });
-
-                expect(sut).toBeDefined();
-            });
-
-            it('should be able to define the module by name WITH dependencies', function () {
-                var sut = global.define(testModuleDefinitions.emptyToo.name, [testModuleDefinitions.empty.name], function (dep) {
-                    return {
-                        depOut: dep,
-                        thisOut: testModuleDefinitions.emptyToo.output
-                    };
-                });
-
-                expect(sut).toBeDefined();
-            });
-
-            it('should be able to define as a factory, anonymously', function (done) {
-                global.define(function (require, exports) {
-                    var actual,
-                        expected = 'tada';
-
-                    exports.tada = expected;
-                    actual = require('tada');
-
-                    expect(actual).toBe(expected);
-
-                    done();
-                });
-            });
-
-            it('should be able to define as an object literal, anonymously', function (done) {
-                var expected = 'anonymous literal';
-
-                global.define({
-                    anonLiteral: expected
-                });
-
-                global.require(['anonLiteral'], function (actual) {
-                    expect(actual).toBe(expected);
-                    done();
-                });
-            });
-
-            it('should be able to define as an object literal, by name', function (done) {
-                var expected = 'anonymous literal';
-
-                global.define('aLiteral', {
-                    aLiteral: expected
-                });
-
-                global.require(['aLiteral'], function (actual) {
-                    expect(actual.aLiteral).toBe(expected);
-                    done();
-                });
-            });
-        }); // /define
-
-        describe('require, when requiring modules', function () {
-            it('should be able to require an array of modules by name', function (done) {
-                global.require([testModuleDefinitions.emptyToo.name], function (result) {
-                    expect(result.depOut).toBe(testModuleDefinitions.empty.output);
-                    expect(result.thisOut).toBe(testModuleDefinitions.emptyToo.output);
-
-                    done();
-                });
-            });
-
-            it('should be able to require single modules by name', function () {
-                var result = global.require(testModuleDefinitions.emptyToo.name);
-
-                expect(result.depOut).toBe(testModuleDefinitions.empty.output);
-                expect(result.thisOut).toBe(testModuleDefinitions.emptyToo.output);
-            });
-
-            // if the second argument of define is a factory that accepts arguments, but
-            // there are no dependencies, then it should not be executed when being resolved
-            // we assume it is a factory
-            it('should be able to require factories by name', function (done) {
-                var expected = 'hello world!';
-
-                container.define('messenger', function (msg) {
-                    return msg;
-                });
-
-                container.require(function (require, exports, module) {
-                    var messenger = require('messenger');
-
-                    expect(messenger(expected)).toBe(expected);
-                    done();
-                });
-            });
-
-            it('should be able to require modules inline', function (done) {
-                global.require(function (require, exports, module) {
-                    var result = require(testModuleDefinitions.emptyToo.name);
-
-                    expect(result.depOut).toBe(testModuleDefinitions.empty.output);
-                    expect(result.thisOut).toBe(testModuleDefinitions.emptyToo.output);
-
-                    done();
-                });
-            });
-
-            it('should grant the factory access to the inner container', function (done) {
-                global.require(function (require, exports, module) {
-                    var actual,
-                        expected = 'lala';
-
-                    exports.lala = expected; // is the same as global.define('lala', function () { return epected; });
-                    actual = require('lala');
-
-                    expect(actual).toBe(expected);
-
-                    done();
-                });
-            });
-        }); // /require
-    });
     
-});
+    require(['window', 'Hilary', 'spec', 'makeMockData', 'createGuid'], function (global, Hilary, spec, makeMockData, generateId) {
+    
+        var scope = new Hilary(),
+            it = spec.it,
+            expect = spec.expect,
+            testModules = makeMockData(scope, generateId);
+
+        spec.describe("Hilary AMD Extensions", function () {
+
+            spec.describe('AMDContainer', function () {
+                it('should exist in window', function () {
+                    expect(global.AMDContainer).to.be.a('object');
+                });
+            });
+
+            spec.describe('define', function () {
+                it('should exist in window', function () {
+                    expect(global.define).to.be.a('function');
+                    expect(global.define.amd).to.be.a('object');
+                });
+    
+                it('should exist in new Hilary instances', function () {
+                    expect(new Hilary().define).to.be.a('function');
+                });
+            });
+
+            spec.describe('require', function () {
+                it('should exist in window', function () {
+                    expect(global.require).to.be.a('function');
+                });
+    
+                it('should exist in new Hilary instances', function () {
+                    expect(new Hilary().require).to.be.a('function');
+                });
+            });
+
+            spec.describe('define, when registering modules', function () {
+                it('should be able to define the module by name WITHOUT dependencies', function () {
+                    // when
+                    global.define(testModules.module1.name, testModules.module1.moduleDefinition.factory);
+                    
+                    // then
+                    var actual = global.require(testModules.module1.name);
+                    expect(actual).to.equal(testModules.module1.expected);
+                });
+    
+                it('should be able to define the module by name WITH dependencies', function () {
+                    global.define(testModules.module2.name, testModules.module2.moduleDefinition.dependencies, testModules.module2.moduleDefinition.factory);
+
+                    var actual = global.require(testModules.module2.name);
+                    expect(actual.thisOut).to.equal(testModules.module2.expected);
+                });
+
+                it('should be able to define as a factory, anonymously', function (done) {
+                    global.define(function (require, exports) {
+                        var actual,
+                            expected = 'tada';
+                        
+                        exports.tada = {
+                            factory: function () {
+                                return expected;
+                            }
+                        };
+                        actual = require(expected);
+
+                        expect(actual).to.equal(expected);
+
+                        done();
+                    });
+                });
+
+                it('should be able to define as an object literal, anonymously', function (done) {
+                    var expected = 'anonymous literal';
+
+                    global.define({
+                        anonLiteral: expected
+                    });
+
+                    global.require(['anonLiteral'], function (actual) {
+                        expect(actual).to.equal(expected);
+                        done();
+                    });
+                });
+
+                it('should be able to define as an object literal, by name', function (done) {
+                    var expected = 'anonymous literal';
+
+                    global.define('aLiteral', {
+                        aLiteral: expected
+                    });
+
+                    global.require(['aLiteral'], function (actual) {
+                        expect(actual.aLiteral).to.equal(expected);
+                        done();
+                    });
+                });
+            }); // /define
+
+            spec.describe('require, when requiring modules', function () {
+                it('should be able to require an array of modules by name', function (done) {
+                    global.require([testModules.module1.name], function (actual) {
+                        expect(actual).to.equal(testModules.module1.expected);
+                        done();
+                    });
+                });
+    
+                it('should be able to require single modules by name', function () {
+                    var actual = global.require(testModules.module2.name);
+                    expect(actual.thisOut).to.equal(testModules.module2.expected);
+                });
+
+                // if the second argument of define is a factory that accepts arguments, but
+                // there are no dependencies, then it should not be executed when being resolved
+                // we assume it is a factory
+                it('should be able to require factories by name', function (done) {
+                    var expected = 'hello world!',
+                        specScope = new Hilary();
+
+                    specScope.define('messenger', function (msg) {
+                        return msg;
+                    });
+
+                    specScope.require(function (require, exports, module) {
+                        var messenger = require('messenger');
+
+                        expect(messenger(expected)).to.equal(expected);
+                        done();
+                    });
+                });
+
+                it('should be able to require modules inline', function (done) {
+                    global.require(function (require, exports, module) {
+                        var actual = require(testModules.module2.name);
+                        expect(actual.thisOut).to.equal(testModules.module2.expected);
+
+                        done();
+                    });
+                });
+
+                it('should grant the factory access to the inner container', function (done) {
+                    global.require(function (require, exports, module) {
+                        var actual,
+                            expected = 'lala';
+                        
+                        exports.lala = {
+                            factory: function () {
+                                return expected;
+                            }
+                        };
+                        actual = require(expected);
+
+                        expect(actual).to.equal(expected);
+
+                        done();
+                    });
+                });
+            }); // /require
+        });
+    });
+};
+
